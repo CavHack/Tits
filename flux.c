7b0e9cd6a89a16cafd4dbee0def4a8c02c954e78
Karl Whitford <cevb@protonmail.com>
2017-11-25 02:18:48 -0800

    Plant the seed
    Increment count and iterate in a forward propagating phase.

---
+++ flux.c
@@ -0,0 +1,13 @@
+#include<assert.h>
+#include<stdlib.h> // for NULL
+
+#include "flux.h"
+#include "gibbs.h"
+
+/*
+ * Increment the counter
+ * (little endian array of bytes)
+ *
+ */
+
+static void increment_counter(genisys_s *flux) {}
